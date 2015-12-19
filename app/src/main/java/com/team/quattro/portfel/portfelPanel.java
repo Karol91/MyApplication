945255242e6c9af971bfba77104d05160c36afff
Karol91 <kmilej@poczta.pl>
2015-12-19 16:57:59 +0100

    panel 1 portfela
    panel 1 portfela

--- app/src/main/java/com/team/quattro/portfel/portfelPanel.java
+++ app/src/main/java/com/team/quattro/portfel/portfelPanel.java
@@ -1,19 +1,50 @@
 package com.team.quattro.portfel;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class portfelPanel extends ActionBarActivity {
 
+    Button btn1Wallet;
+    Button btn2Wallet;
+    Button btn3Wallet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfel_panel);
-    }
+        btn1Wallet = (Button) findViewById(R.id.imgBtn1Wallet);
+        btn2Wallet = (Button) findViewById(R.id.imgBtn2Wallet);
+        btn3Wallet = (Button) findViewById(R.id.imgBtn3Wallet);
+        btn1Wallet.setOnClickListener(myOnlyhandler);
+        btn2Wallet.setOnClickListener(myOnlyhandler);
+        btn3Wallet.setOnClickListener(myOnlyhandler);
 
+    }
+    View.OnClickListener myOnlyhandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intentWallet;
+            switch(v.getId()) {
+                case R.id.imgBtn1Wallet:
+                    intentWallet = new Intent(portfelPanel.this,wallet1.class);
+                    startActivity(intentWallet);
+                    break;
+                case R.id.imgBtn2Wallet:
+                    intentWallet = new Intent(portfelPanel.this,LoginActivity.class);
+                    startActivity(intentWallet);
+                    break;
+                case R.id.imgBtn3Wallet:
+                    intentWallet = new Intent(portfelPanel.this,LoginActivity.class);
+                    startActivity(intentWallet);
+                    break;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
